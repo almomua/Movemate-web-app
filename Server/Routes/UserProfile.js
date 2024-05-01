ca70e7c0cb38a3932ccb4eb74c71a61c64185faf
sherif harby <96632090+sherifharby@users.noreply.github.com>
2024-05-02 00:50:50 +0300

    Add files via upload

---
+++ Server/Routes/UserProfile.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../Models/UserSchema')
+const errorHandler = require('../Middlewares/errorMiddleware');
+const authTokenHandler = require('../Middlewares/checkAuthToken');
+
+
+function createResponse(ok, message, data) {
+    return {
+        ok,
+        message,
+        data,
+    };
+}
+
+router.put('/quiz', authTokenHandler, async (req, res, next) => {
+    // console.log(req.body);
+    try {
+        const userId = req.userId;
+        let user = await User.findById({_id : userId});
+        if (!user) {
+            return res.status(400).json(createResponse(false, 'Invalid credentials'));
+        }
+        const { gender, dob, goal, activityLevel} = req.body;
+        user = await User.findOneAndUpdate({
+                gender,
+                dob,
+                goal,
+                activityLevel
+            });
+
+        await user.save();
+
+        res.status(201).json(createResponse(true, 'data saved successfully', {user}));
+       
+    }
+    catch (err) {
+        next(err);
+    }
+})
+
+router.use(errorHandler)
+
+module.exports = router;
